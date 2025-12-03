17766ac82102d93545447f0d5ceb4959493a2b8b
Mark Kevin Cacho <147457900+markkev1n@users.noreply.github.com>
2025-12-04 00:04:21 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "pinventory";
+$servername = "t681ln.h.filess.io";
+$username = "pinventory_stemshisor";
+$password = "45350513e8c96f8a9f06239f2c8ebd125d72438a";
+$database = "pinventory_stemshisor";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
@@ -10,3 +10,4 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
